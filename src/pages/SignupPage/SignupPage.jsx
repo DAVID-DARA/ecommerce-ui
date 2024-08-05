df488a3c429bc1bcb00a55c3d1dd651b835718ed
DAVID-DARA <davidfamoyegun1@gmail.com>
2024-08-05 17:37:14 +0100

    Changed from regular css to css modules, Added form validation using formik, Added error page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,102 +1,113 @@
-import "./SignupPage.css"
-import Logo_Header from "../../components/logo-header/Logo";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Link, redirect } from "react-router-dom";
+
+import styles from "./SignupPage.module.css";
+import Logo_Header from "../../components/logo-header/Logo";
 import google from "../../assets/new-icons/google.png";
+import LoadingIndicator from "../../components/LoadingIndicator/LoadingIndicator";
 
 const SignupPage = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
-    
-    const signupRequestDto = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (error) {
+            const timer = setTimeout(() => setError(""), 10000);
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const { firstName, lastName, email, password } = formData;
+
+    const validateForm = () => {
+        if (!firstName || !lastName || !email || !password) {
+            setError("All fields are required.");
+            return false;
+        }
+        return true;
     };
 
     const sendRequest = () => {
+        if (!validateForm()) return;
+
+        setLoading(true);
+        setError("");
+
         axios
-            .post("http://localhost:4032/api/user/signup", signupRequestDto)
-            .then((response => {
-                const signupResposne = response.data;
-                console.log(signupResposne);
-
-                if (response.status == 201) {
-                    redirect("/home")
-                    console.log("Successful Signup")
+            .post("http://localhost:4032/api/user/signup", formData)
+            .then((response) => {
+                if (response.status === 201) {
+                    localStorage.setItem("userEmail", email);
+                    navigate('/verify');
                 } else {
-                    console.log("Failed Signup")
-                    console.log()
+                    setError("Signup failed. Please try again.");
                 }
-            }))
-    }
-
-    function firstNameChange(event) {
-        setFirstName(event.target.value);
-    }
-
-    function lastNameChange(event) {
-        setLastName(event.target.value)
-    }
-
-    function emailChange(event) {
-        setEmail(event.target.value)
-    }
-
-    function passwordChange(event) {
-        setPassword(event.target.value)
-    }
+            })
+            .catch((error) => {
+                console.error("There was an error during the signup request:", error);
+                setError("Signup failed. Please try again.");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
     return (
-        <>
-            <div className="signup">
-                <div className="section">
-                    <div className="signup-left">
-                        <div className="content">
-                            <Logo_Header />
-                            <p className="head">Sign up</p>
-                            <p className="text-one">Fill in information below or register with your social account</p>
-                            <div className="name-form">
-                                <div className="labels">
-                                    <span className="signup-labels">First Name *</span><br />
-                                    <input type="text" placeholder="Enter First Name" onChange={firstNameChange} value={firstName} className="name" />
-                                </div>
-                                <div className="labels">
-                                    <span className="signup-labels">Last Name *</span><br />
-                                    <input type="text" placeholder="Enter Last Name" onChange={lastNameChange} value={lastName} className="name" /><br />
-                                </div>
+        <div className={styles.signup}>
+            <div className={styles.section}>
+                <div className={styles.signupLeft}>
+                    <div className={styles.signupContent}>
+                        <Logo_Header />
+                        <p className={styles.head}>Sign up</p>
+                        <p className={styles.textOne}>Fill in information below or register with your social account</p>
+                        <div className={styles.nameForm}>
+                            <div className={styles.labels}>
+                                <label className={styles.signupLabels} htmlFor="firstName">First Name *</label><br />
+                                <input type="text" id="firstName" name="firstName" placeholder="Enter First Name" onChange={handleChange} value={firstName} className={styles.name} required />
                             </div>
-                            <span className="signup-labels">Email *</span><br />
-                            <input className="signup-input" type="text" placeholder="Email" onChange={emailChange} value={email}/> <br />
-                            <span className="signup-labels">Password *</span><br />
-                            <input className="signup-input" type="password" placeholder="Password" onChange={passwordChange} value={password}/> <br />
-                            <div className="terms">
-                                <input className="signup-input" type="checkbox" name="terms" /><span className="signup-checkbox-text" > &nbsp; Agree with <Link to="/">Terms & Condition</Link> and Privacy Policy</span>
+                            <div className={styles.labels}>
+                                <label className={styles.signupLabels} htmlFor="lastName">Last Name *</label><br />
+                                <input type="text" id="lastName" name="lastName" placeholder="Enter Last Name" onChange={handleChange} value={lastName} className={styles.name} required /><br />
                             </div>
-                            {/* <a href="/signup-page">Forgot Password</a> */}
-                            <input className="signup-input signup-button" type="button" value="Sign up" onClick={sendRequest} />
-
-                            <div className="signup-hr">
-                                <hr className="signup-hr-line" />&nbsp;&nbsp;
-                                <span className="signup-hr-text">or Sign Up with</span>&nbsp;&nbsp;
-                                <hr className="signup-hr-line"/>
-                            </div>
-
-                            <button className="signup-google"><img src={google} alt=""  width="13px"/>&nbsp;Sign Up With Google</button>
-                            <p className="signup-signin-link">Already have an account? <Link to="/login">Sign In</Link></p>
-
                         </div>
-                    </div>
-                    <div className="signup-right">
+                        <label className={styles.signupLabels} htmlFor="email">Email *</label><br />
+                        <input className={styles.signupInput} type="email" id="email" name="email" placeholder="Email" onChange={handleChange} value={email} required /> <br />
+                        <label className={styles.signupLabels} htmlFor="password">Password *</label><br />
+                        <input className={styles.signupInput} type="password" id="password" name="password" placeholder="Password" onChange={handleChange} value={password} required /> <br />
+                        {error && <p style={{ color: 'red', textAlign: "center", fontSize: "12px" }}>{error}</p>}
+                        <div className={styles.terms}>
+                            <input className={styles.signupInput} type="checkbox" name="terms" /><span className={styles.signupCheckboxText}> &nbsp; Agree with <Link to="/">Terms & Condition</Link> and Privacy Policy</span>
+                        </div>
+                        <button className={`${styles.signupInput} ${styles.signupButton}`} onClick={sendRequest}>{loading ? <LoadingIndicator size={14} /> : "Sign up"}</button>
+                        <div className={styles.signupHr}>
+                            <hr className={styles.signupHrLine} />&nbsp;&nbsp;
+                            <span className={styles.signupHrText}>or Sign Up with</span>&nbsp;&nbsp;
+                            <hr className={styles.signupHrLine} />
+                        </div>
+                        <button className={styles.signupGoogle}><img src={google} alt="" width="13px" />&nbsp;Sign Up With Google</button>
+                        <p className={styles.signupSigninLink}>Already have an account? <Link to="/login">Sign In</Link></p>
                     </div>
                 </div>
+                <div className={styles.signupRight}></div>
             </div>
-        </>
+        </div>
     );
 }
 
