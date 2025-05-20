c0d6c4bf87e0ea71bc9f3a6f28de8fcd08efc0e7
DAVID-DARA <davidfamoyegun1@gmail.com>
2025-05-20 07:53:04 +0100

    Navbar design completed

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,6 @@
+import user from "./user.png";
+import search from "./search.png";
+import wishlist from "./wishlist.png";
+import cart from "./cart.png";
+
+export default {user, search, wishlist, cart }
--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,30 +1,74 @@
 import { Link } from "react-router";
 import Logo from "../logo/Logo";
 import styles from "./Navbar.module.css";
+import icons from "../../../assets/icons/icons";
 
 const Navbar = () => {
   return (
     <>
       <div className={styles.navbar}>
         <div className={styles.navItems}>
-          <div className="left">
+          <div className={styles.left}>
             <Logo />
           </div>
-          <div className="middle">
+          <div className={styles.middle}>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <Link className={styles.link_deco} to="/">
+                  Home
+                </Link>
               </li>
-              <li>Shop</li>
-              <li>Women</li>
-              <li>Men</li>
-              <li>Accessories</li>
-              <li>About Us</li>
-              <li>Contact Us</li>
-              <li>Blog</li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Women
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Men
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Accessories
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link_deco} to="/">
+                  Blog
+                </Link>
+              </li>
             </ul>
           </div>
-          <div className="right"></div>
+          <div className={styles.right}>
+            <Link className={styles.link_deco} to="/">
+              <img src={icons.search} width="20px" />
+            </Link>
+            <Link className={styles.link_deco} to="/">
+              <img src={icons.wishlist} width="20px" />
+            </Link>
+            <Link className={styles.link_deco} to="/">
+              <img src={icons.cart} width="20px" />
+            </Link>
+            <Link className={styles.link_deco} to="/">
+              <img src={icons.user} width="20px" />
+            </Link>
+          </div>
         </div>
       </div>
     </>
